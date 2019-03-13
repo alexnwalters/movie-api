dd1c57248ab5d5743d0da866abb2349b4aa499c5
Alex Walters <alexnwalters@gmail.com>
2019-03-13 18:52:07 -0400

    fix api request to https, amend get display

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'Use Strict';
 
 const omdbKey = 'ca762609';
-const omdbURL = 'http://www.omdbapi.com/?';
+const omdbURL = 'https://www.omdbapi.com/?';
 
 function formatParams(params) {
     const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -22,7 +22,7 @@ function runMoviesGets(movie1, movie2) {
     const searchMovie1String = formatParams(params1);
     const searchMovie1URL = omdbURL + searchMovie1String;
     const searchMovie2String = formatParams(params2);
-    const searchMovie2URL = omdbURL + searchMovie2String;
+    const searchMovie2URL = omdbURL + searchMovie2String;  
 
     getMoviesForDisplay(searchMovie1URL, searchMovie2URL);
     getMoviesRatings(searchMovie1URL, searchMovie2URL);
@@ -71,21 +71,42 @@ function displayResults(responseJson, movieDiv) {
         <ul class='js-movie-rating'>`
     );
 
-    for (let i = 0; i < responseJson.Ratings.length; i++) {
+    if (responseJson.Ratings.length == 2) {
         $(movieDiv).append(
-            `<li>${responseJson.Ratings[i].Source}: ${responseJson.Ratings[i].Value}</li>`
+            `<li>${responseJson.Ratings[0].Source}: ${responseJson.Ratings[0].Value}</li>`
         );
-    };
+
+        if (responseJson.Ratings[1].Source == "Rotten Tomatoes") {
+            $(movieDiv).append(
+                `<li>${responseJson.Ratings[1].Source}: ${responseJson.Ratings[1].Value}</li>
+                <li>Metacritic: No Score</li>`
+            );
+        }
+        else {
+            $(movieDiv).append(
+                `<li>Rotten Tomatoes: No Score</li>
+                <li>${responseJson.Ratings[1].Source}: ${responseJson.Ratings[1].Value}</li>`
+            );
+        }
+    }
+    else {
+        for (let i = 0; i < responseJson.Ratings.length; i++) {
+            $(movieDiv).append(
+                `<li>${responseJson.Ratings[i].Source}: ${responseJson.Ratings[i].Value}</li>`
+            );
+        }
+    }
 
     $(movieDiv).append(
         '</ul>'
     );
 }
 
-function getMoviesRatings (searchMovie1URL, searchMovie2URL) { 
-    const movie1Scores = [];
-    const movie2Scores = [];
+const movie1Scores = [];
+const movie2Scores = [];
 
+function getMoviesRatings (searchMovie1URL, searchMovie2URL) {
+    
     fetch(searchMovie1URL)
         .then(response => {
             if (response.ok) {
@@ -105,20 +126,17 @@ function getMoviesRatings (searchMovie1URL, searchMovie2URL) {
             }
             throw new error(response.statusText);
         })
-        .then(responseJson => storeRatings(responseJson, movie2Scores))
+        .then(responseJson => storeRatings(responseJson, movie1Scores))
         .catch(err => {
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
-
-    console.log(movie1Scores);
 } 
 
 function storeRatings(responseJson, scores) {
     scores = responseJson.Ratings;
-    
     console.log(scores);
     return scores;
-}
+} 
 
 function watchForm() {
     $('form').submit(event => {
