7792ee8ab4abdaf77e3a7bd8b1463c86ad6993b2
Alex Walters <alexnwalters@gmail.com>
2019-03-14 00:29:43 -0400

    add get for tmdb find id and tmdb get trailer

--- script.js
+++ script.js
@@ -3,25 +3,29 @@
 const omdbKey = 'ca762609';
 const omdbURL = 'https://www.omdbapi.com/?';
 
+const tmdbKey = '572565d80f61af6afed1df98562c0e06';
+const findURL = 'https://api.themoviedb.org/3/find/';
+const trailerURL = 'https://api.themoviedb.org/3/movie/'
+
 function formatParams(params) {
     const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
 }
 
 function runMoviesGets(movie1, movie2) {
-    const params1 = {
+    const omdbParams1 = {
         apikey: omdbKey,
         t: movie1
     };
 
-    const params2 = {
+    const omdbParams2 = {
         apikey: omdbKey,
         t: movie2
     };
 
-    const searchMovie1String = formatParams(params1);
+    const searchMovie1String = formatParams(omdbParams1);
     const searchMovie1URL = omdbURL + searchMovie1String;
-    const searchMovie2String = formatParams(params2);
+    const searchMovie2String = formatParams(omdbParams2);
     const searchMovie2URL = omdbURL + searchMovie2String;  
 
     getMoviesForDisplay(searchMovie1URL, searchMovie2URL);
@@ -108,6 +112,8 @@ function displayResults(responseJson, movieDiv, scores) {
     storeRatings(responseJson, scores)
     
     console.log(scores);
+
+    runTmdbFindGet(responseJson);
 }
 
 function storeRatings(responseJson, scores) {
@@ -117,6 +123,43 @@ function storeRatings(responseJson, scores) {
         scores.push(responseJson.Ratings[i]);
     };
     return scores;
+}
+
+function runTmdbFindGet(omdbResponse) {
+    const tmdbParamFind = {
+        api_key: tmdbKey,
+        language: 'en-US',
+        external_source: 'imdb_id',
+    }
+
+    const tmdbFindURL = findURL + omdbResponse.imdbID + '?' + formatParams(tmdbParamFind);
+
+    getUseImdbIdtoFindTmdbId(tmdbFindURL);
+}
+
+function getUseImdbIdtoFindTmdbId(tmdbFindURL) {
+    fetch(tmdbFindURL)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new error(response.statusText);
+        })
+        .then(responseJson => runTmdbTrailerGet(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function runTmdbTrailerGet(responseJson) {
+    const tmdbParamTrailer = {
+        api_key: tmdbKey,
+        language: 'en-US',
+    }
+
+    const tmdbTrailerURL = trailerURL + responseJson.movie_results[0].id + '/videos?' + formatParams(tmdbParamTrailer);
+
+    console.log(tmdbTrailerURL);
 }
 
 function watchForm() {
